f3bacc93b1620ef439d0ebb813c207d9a9a3ae8d
Harald Pehl <harald.pehl@gmail.com>
2021-02-23 13:04:51 +0100

    React sample

---
+++ react/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    kotlin("js") version "1.4.30"
+}
+
+group = "com.redhat.kotlinjs"
+version = "0.0.1"
+
+repositories {
+    jcenter()
+    mavenCentral()
+    maven { url = uri("https://dl.bintray.com/kotlin/kotlin-js-wrappers") }
+}
+
+dependencies {
+    implementation("org.jetbrains:kotlin-react:16.13.1-pre.113-kotlin-1.4.0")
+    implementation("org.jetbrains:kotlin-react-dom:16.13.1-pre.113-kotlin-1.4.0")
+    implementation("org.jetbrains:kotlin-styled:1.0.0-pre.113-kotlin-1.4.0")
+}
+
+kotlin {
+    js {
+        browser {
+            commonWebpackConfig {
+                cssSupport.enabled = true
+            }
+            binaries.executable()
+        }
+    }
+}
